48fa8e6fa5d89d38204edcb3542e4fdf403796df
Ludwisia1 <Ludwisia123456@example.com>
2022-04-19 21:41:43 +0200

    Commit1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String month = "April";
-        int year = 2022;
-        long day = 19;
-        System.out.println("Miesiąc: "+ month);
-        System.out.println("Rok: "+ year);
-        System.out.println("Dzień: "+ day);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Jak masz na imię?");
+        String name = scanner.nextLine();
+        System.out.println("Cześć, " + name);
+
+        System.out.println("Jak masz na nazwisko?");
+        String lastname = scanner.nextLine();
+        System.out.println("Twoje nazwisko to: " + lastname);
+
+        System.out.println("Ile masz lat?");
+        String age = scanner.nextLine();
+
+        System.out.println("Jaką mamy dziś datę?");
+        String data = scanner.nextLine();
+
+        System.out.println("Cześć, " + name + " " + lastname + " masz " + age + " lat. A dzisiejsza data to: " + data);
+
+
     }
 }
+
