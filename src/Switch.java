76a39cb5f21cfbf310b2e71a37f802eff4b9bbcf
Ludwisia1 <Ludwisia123456@example.com>
2022-04-20 21:00:19 +0200

    Java Basic Awsome training2

---
+++ src/Switch.java
@@ -0,0 +1,33 @@
+public class Switch {
+    public static void main(String[] args) {
+        int a = 11;
+        switch (a) {
+            case 1:
+                System.out.println("poniedziałek");
+                break;
+            case 2:
+                System.out.println("wtorek");
+                break;
+            case 3:
+                System.out.println("środa");
+                break;
+            case 4:
+                System.out.println("czwartek");
+                break;
+            case 5:
+                System.out.println("piątek");
+                break;
+            case 6:
+                System.out.println("sobota");
+                break;
+            case 7:
+                System.out.println("niedziela");
+                break;
+
+            default:
+                System.out.println("papa");
+
+
+        }
+    }
+}
